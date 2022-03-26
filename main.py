7147b57833b4ac0862ca4fc64136a8d7323012d7
Justin Nguyen <sonerdvn@gmail.com>
2022-03-26 12:14:05 +0000

    Them api

--- main.py
+++ main.py
@@ -3,9 +3,18 @@ from datetime import datetime
 import pytz
 from datetime import date
 from discord.ext import commands
-
+import requests
 #chỉnh prefix
 prefix="?"
+
+#dùng api của openweather để lấy thông tin thời tiết
+api_request ='http://api.openweathermap.org/data/2.5/weather?q=dalat&appid=cee343d33e41970dd63c44b39c8620ab'
+data = requests.get(api_request).json()
+format_data = data['main']
+
+a = "Nhiệt độ của thành phố Đà Lạt hôm nay có nhiệt độ thấp nhất là {0}, nhiệt độ cao nhất là {1} Độ C".format(
+int(format_data['temp_min']-273),int(format_data['temp_max']-273))
+
 
 #chỉnh format của ngày tháng năm
 date_format = "%y/%m/%d"
@@ -32,6 +41,12 @@ async def on_message(message):
     if message.content.startswith("hello"):
         await message.channel.send("hi")
     await bot.process_commands(message)
+    if message.content.startswith("goodnight"):
+        await message.channel.send("Goodnight")
+
+  
+
+  
   
 #command của bot
 @bot.command()
@@ -46,6 +61,11 @@ async def monthrn(ctx):
 @bot.command()
 async def yearrn(ctx):
   await ctx.send(year_rn)
+@bot.command()
+async def nhietdodl(ctx):
+  await ctx.send(a)
+
+   
  
 #chạy bot
-bot.run(token)
+bot.run("OTU3MTUzNjM2MDYzMDEwODM3.Yj6oyQ.9Pf7DRtwihdpSVj-iErozsOZDVs")
