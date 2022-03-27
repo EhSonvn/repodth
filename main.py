a4dc2f4a3e41324c87a4d1079a5ee9a61e8cd3a3
Sơn <85204262+EhSonvn@users.noreply.github.com>
2022-03-27 07:43:32 +0000

    Chỉnh lại api và thêm từ

--- main.py
+++ main.py
@@ -13,11 +13,17 @@ bot = commands.Bot(command_prefix = prefix)
 
 #khai báo biến ngày tháng năm và dùng múi giờ việt nam
 my_time =datetime.now(pytz.timezone('Asia/Ho_Chi_Minh')).time()
+
 my_time = datetime.now(pytz.timezone('Asia/Ho_Chi_Minh')).time()
+
 my_date = date.today()
+
 date_rn = my_date.strftime("%d")
+
 month_rn = my_date.strftime("%m")
+
 year_rn = my_date.strftime("%Y")
+
 #event của bot
 @bot.event
 async def on_ready():
@@ -28,32 +34,57 @@ async def on_ready():
 @bot.command()
 async def khoidong(ctx):
   await ctx.send("Bot đã sẵn sàng!")
+  
 @bot.command()
 async def dayrn(ctx):
   await ctx.send(date_rn)
+  
 @bot.command()
 async def timern(ctx):
   await ctx.send(my_time)
+  
 @bot.command()
 async def monthrn(ctx):
   await ctx.send(month_rn)
+  
 @bot.command()
 async def yearrn(ctx):
   await ctx.send(year_rn)
+  
 @bot.command()
 async def xemtt(ctx, arg):
-  user_api = "dbea7ad7feadaf1076361fe27785793c"
+  user_api = os.getenv('weather_api')
   a = arg
-  user_api = "dbea7ad7feadaf1076361fe27785793c"
   api_add = "https://api.openweathermap.org/data/2.5/weather?q="+a+"&appid="+user_api+"&lang=vi"
+  
   api_data = requests.get(api_add).json()
+  
   temp_city = ((api_data['main']['temp']) - 273.15)
+  
   weather_desc = api_data['weather'][0]['description']
+  
   hmdt = api_data['main']['humidity']
+  
   wind_spd = api_data['wind']['speed']
+  
   city_name =  api_data['name']
+  
   report = "Tình hình thời tiết tại {0}: {1}\n Nhiệt độ: {2}\n Tốc độ gió: {3}m/s\n Độ ẩm: {4}%".format(city_name, weather_desc, temp_city, wind_spd, hmdt)
   await ctx.send(report)
+  
+@bot.command()
+async def define(ctx, arg):
+  words = arg
+  
+  api_address = "https://api.dictionaryapi.dev/api/v2/entries/en/"+words
+  
+  get_def = requests.get(api_address).json()
+  
+  word_def =  get_def[0]['word']
+  
+  word_def2 =  get_def[0]['meanings'][0]['definitions'][0]['definition']
+  a = "Word: {0}\n Definition: {1}\n".format(word_def, word_def2)
+  await ctx.send(a)
     
 
 #chạy bot
