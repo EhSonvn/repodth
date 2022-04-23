e8d12e436ec3a5a0d1f7a7699b0f2316e7b88395
Sơn <85204262+EhSonvn@users.noreply.github.com>
2022-04-23 05:00:12 +0000

    Thêm vào các lỗi api để gia tăng trải nghiệm người dùng

--- main.py
+++ main.py
@@ -67,28 +67,41 @@ async def yearrn(ctx):
 #xem thời tiết
 @bot.command()
 async def xemtt(ctx, arg1, arg2='', arg3=''):
-  user_api = os.getenv('weather_api')
-  if arg3 != '':
-    complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+arg1 + "%20" + arg2 + "%20" + arg3 +"&appid="+user_api+"&lang=vi"
-  else:
-    complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+arg1 + "%20" + arg2 +"&appid="+user_api+"&lang=vi"
-  api_data = requests.get(complete_api_link).json()
-  temp_city = round((api_data['main']['temp'] - 273.15), 1)
-  weather_desc = api_data['weather'][0]['description']
-  hmdt = api_data['main']['humidity']
-  wind_spd = round(api_data['wind']['speed'] * 3,6)
-  city = api_data['name']
-  country = api_data['sys']['country']
-  embed = discord.Embed(title='Thông tin về thời tiết tại {0} thuộc {1}'.format(city, country), colour=discord.Color.blue())
-  embed.add_field(name="Nhiệt độ", value="{0} độ C".format(temp_city),inline=True)
-  embed.set_footer(text='Dữ liệu từ openweathermap.org')
-  embed.add_field(name='Tình hình', value=weather_desc, inline=True)
-  embed.add_field(name="Độ ẩm", value="{0}%".format(hmdt), inline=True)
-  embed.add_field(name="Tốc độ gió", value='{0}km/h'.format(wind_spd), inline=True)
-  await ctx.send(embed=embed)
-  
-
-#xem định nghĩa của 1 từ nào đó
+  api_key = os.getenv('weather_api')
+  link = 'http://api.weatherapi.com/v1/current.json?key=' + api_key + '&q=' + arg1 + '%20' + arg2 + '%20' + arg3 + '&lang=en'
+  api = requests.get(link)
+  data = api.json()
+  if api.status_code == 200:
+    city_temp = round(data['current']['temp_c'], 2)
+    city_desc = data['current']['condition']['text']
+    city = data['location']['name']
+    last_update = data['current']['last_updated']
+    wind_direction = data['current']['wind_dir']
+    country = data['location']['country']
+    humidity = data['current']['humidity']
+    wind_speed = round(data['current']['wind_kph'] / 3.6, 2)
+    embed = discord.Embed(
+        title='Thông tin về thời tiết tại {0} thuộc {1}'.format(
+            city, country),
+        description='Dữ liệu mới nhật được cập nhật vào lúc {0}'.
+        format(last_update),
+        colour=discord.Color.blue())
+    embed.add_field(name="Nhiệt độ",
+                    value="{0} độ C".format(city_temp),
+                    inline=True)
+    embed.set_footer(text='Dữ liệu từ weatherapi.')
+    embed.add_field(name='Tình hình', value=city_desc, inline=True)
+    embed.add_field(name="Độ ẩm", value="{0}%".format(humidity), inline=True)
+    embed.add_field(name="Hướng gió", value=wind_direction, inline=True)
+    embed.add_field(name="Tốc độ gió",
+                    value='{0}m/s'.format(wind_speed),
+                    inline=True)
+    await ctx.send(embed=embed)
+  elif api.status_code == 500:
+    await ctx.send("Lỗi server, mời bạn thử lại lần sau!")
+  elif api.status_code == 400:
+    await ctx.send("Thành phố không tồn tại! Bạn đã gõ đúng chính tả chưa?")
+  #xem định nghĩa của 1 từ nào đó
 @bot.command()
 async def define(ctx, arg4, arg5=''):
     words = arg4
