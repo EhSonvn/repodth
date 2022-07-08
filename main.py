5490b664274fce316f55c106b9a1b909877e3b9b
Justin <sonerdvn@gmail.com>
2022-07-08 08:29:31 +0700

    Stuff

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ start_time = time.time()
  
 prefix = ">"
 
-bot = commands.Bot(command_prefix=prefix)
+bot = commands.Bot(command_prefix=prefix, intents=discord.Intents.all())
 
 for file in os.listdir("commands"):
     if file.endswith(".py"):
@@ -15,8 +15,7 @@ for file in os.listdir("commands"):
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(status=discord.Status.online,
-                              activity=discord.Game('Happy Pride Month!'))
+    await bot.change_presence(status=discord.Status.online)
     print('We have logged in as {0.user}'.format(bot))  
    
 
