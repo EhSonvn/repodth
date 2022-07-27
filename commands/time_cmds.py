82937f71f9900bb518735074ee6f75c55787668f
Sơn <85204262+EhSonvn@users.noreply.github.com>
2022-07-27 07:55:35 +0700

    Update time_cmds.py

--- commands/time_cmds.py
+++ commands/time_cmds.py
@@ -14,19 +14,19 @@ class TCmd(commands.Cog):
         await ctx.send("Hello!")
 
     @commands.command()
-    async def day_rn(self, ctx):
+    async def dayrn(self, ctx):
         await ctx.send(datetime.now(pytz.timezone('Asia/Ho_Chi_Minh')).strftime("%d"))
 
     @commands.command()
-    async def month_rn(self, ctx):
+    async def monthrn(self, ctx):
         await ctx.send(datetime.now(pytz.timezone('Asia/Ho_Chi_Minh')).strftime("%m"))
 
     @commands.command()
-    async def year_rn(self, ctx):
+    async def yearrn(self, ctx):
         await ctx.send(datetime.now(pytz.timezone('Asia/Ho_Chi_Minh')).strftime("%Y"))
 
     @commands.command()
-    async def time_rn(self, ctx):
+    async def timern(self, ctx):
         await ctx.send(datetime.fromtimestamp(time.time() + 25200).strftime('%H:%M:%S'))
 
 
