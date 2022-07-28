bd0e4a84fd286d881287d3d26c2700c04dc100bc
Justin <sonerdvn@gmail.com>
2022-07-28 17:10:36 +0700

    Added forecast feature

--- commands/weather.py
+++ commands/weather.py
@@ -1,3 +1,4 @@
+from sys import api_version
 import discord
 from discord.ext import commands
 import requests
@@ -54,6 +55,49 @@ class Weather(commands.Cog):
             await ctx.send(
                 "Truy cập bị cấm, xin hãy thử lại lần sau! Mã lỗi: {0}".format(
                     api.status_code))
+
+    @commands.command()
+    async def dubao(self, ctx, day, arg1, arg2='', arg3=''):
+        link = "https://api.weatherapi.com/v1/forecast.json?key={0}&q=" + arg1 + '%20' + arg2 + '%20' + arg3 +"&days={1}&aqi=no&alerts=no".format(os.getenv('weather_api'), day)
+        api = requests.get(link)
+        data = api.json()
+        if api.status_code == 200:
+            city = data['location']['name']
+            country = data['location']['country']
+            last_update = data['current']['last_updated']
+            embed1 = discord.Embed(title="Thông tin dự báo thời tiết tại {0} thuộc {1}", description="Được cấp nhật lần cuối vào lúc {2}").format(city, country, last_update)
+            
+            for i in range(0, day+1):
+                embed = discord.Embed(title="Vào ngày {1}").format(data['forecast']['forecastday'][i]['day'])
+                embed.add_field(name="Nhiệt độ thấp nhất: {0}\n Nhiệt độ cao nhất: {1}").format(data['forecast']['forecastday'][i]['day']['mintemp_c'], data['forecast']['forecastday'][i]['day']['maxtemp_c'])
+                embed.add_filed(name="Tốc độ gió cao nhất: {1}").format(round(data['forecast']['forecastday'][i]['day']['maxwind_kph']/3.6))
+                embed.add_field(name="Độ ẩm trung bình: {1}").format(data['forecast']['forecastday'][i]['day']['avghumidity'])
+                await ctx.send(embed)
+            
+            await ctx.send(embed1)
+        
+        elif api.status_code == 500:
+            await ctx.send(
+                "Lỗi server, mời bạn thử lại lần sau! Mã lỗi: {0}".format(
+                    api.status_code))
+        elif api.status_code == 400 or api.status_code == 404:
+            await ctx.send(
+                "Thành phố không tồn tại! Bạn đã gõ đúng chính tả chưa? Mã lỗi: {0}"
+                .format(api.status_code))
+        elif api.status_code == 403:
+            await ctx.send(
+                "Truy cập bị cấm, xin hãy thử lại lần sau! Mã lỗi: {0}".format(
+                    api.status_code))
+
+
+    
+
+            
+            
+
+
+
+
 
 
 def setup(bot):
